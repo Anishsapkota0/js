13fd7bca58179287491dcd1063246175275c01a6
Anish sapkota <anishsapkota265@gmail.com>
2026-06-11 13:57:27 +0545

    js introduction

---
+++ index.js
@@ -0,0 +1,19 @@
+console.log("Hello world")
+var fullName= "anish sapkota";
+console.log(fullName)
+//camelCase
+//snake_case
+//PascalCase
+fullName="anish"
+console.log(fullName)
+
+let email ="ansihsapkota265@gmail.com"
+//let email =""   //error
+console.log(email)
+{
+    let email="abc@gmail.com"
+    console.log(email)
+}
+console.log(email)
+
+const ROLE="trainer"    //non changing data
