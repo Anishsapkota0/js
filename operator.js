5b1d64691637036847cb6f4a809948280846d353
Anish sapkota <anishsapkota265@gmail.com>
2026-06-12 18:11:27 +0545

    operators added

---
+++ operator.js
@@ -0,0 +1,56 @@
+/**
+ * Arithmetic / Mathemetical operators
+ */
+let a = 100;
+let b= 30;
+let div = a/b 
+console.log(div)
+
+/** increment and decrement operators */
+
+
+/**
+ * Assignment Operators
+ *  =, +=. -=, /=. *=
+ */
+
+/** 
+ * String / concatination operatord
+ * +
+ * 
+ */
+let e = 10;
+let f = "10";
+
+console.log(e-f)
+
+/**
+ * comparision operttord
+ * <, >, <=, >=,==,===,!=, !==
+ * boolean
+ */
+console.log(e<f)
+console.log(e<=f)
+console.log(e>f)
+console.log(e>=f)
+console.log(e==f)
+console.log(e===f)
+console.log(e!=f)
+console.log(e!==f)
+
+/**
+ * Logical operators
+ * &&, !, || 
+ * compares two conditions
+ */
+
+
+/**
+ * conditional operators or ternery operator
+ * (exp)? true : false
+ */
+
+/**
+ * Tempelate literals 
+ * `${name}`
+ */
