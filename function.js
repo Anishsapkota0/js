a8720f6ddfca869ba6c666ffeafbed3aa49df1c7
Anish sapkota <anishsapkota265@gmail.com>
2026-06-18 23:27:17 +0545

    thursday 2nd commit

--- function.js
+++ function.js
@@ -9,6 +9,24 @@ function sayMyName() {
 
 
 function addTwoNumbers(number1,number2){
-   console.log(number1+number2)
+   let result = (number1+number2)
+   return result
 }
-addTwoNumbers(2,3)
+let r = addTwoNumbers(2,30)
+console.log(r)
+
+function calculator(num1,num2){
+    let addition = num1 + num2
+    let subtraction=num1 - num2
+    let multiplication =num1 * num2
+    let division =num1/num2
+    let moduloDivision=num1%num2
+    let result =[`addition of ${num1} and ${num2} is  ${addition}`,
+                  `subtraction of ${num1} and ${num2} is  ${subtraction}`,
+                  `multiplication of ${num1} and ${num2} is  ${multiplication}`,
+                  `Division of ${num1} and ${num2} is  ${division}`,
+                  `ModuloDivision of ${num1} and ${num2} is  ${moduloDivision}`]
+    return result
+}
+let calculatedValue =calculator(6,2)
+console.log(calculatedValue)
