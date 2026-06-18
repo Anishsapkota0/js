d01164dea30455a080bdfeb3620b82b1252b6118
Anish sapkota <anishsapkota265@gmail.com>
2026-06-18 17:41:25 +0545

    Thursday 1st commit functions

---
+++ function.js
@@ -0,0 +1,14 @@
+function sayMyName() {
+    console.log("A")
+    console.log("N")
+    console.log("I")
+    console.log("S")
+    console.log("H")
+    console.log("--------------")
+}
+
+
+function addTwoNumbers(number1,number2){
+   console.log(number1+number2)
+}
+addTwoNumbers(2,3)
