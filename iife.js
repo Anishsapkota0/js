b39f3852e36036e50d700816d89356515aa78ee9
Anish sapkota <anishsapkota265@gmail.com>
2026-06-23 22:23:17 +0545

    tuesday 3rd commit

--- iife.js
+++ iife.js
@@ -1,7 +1,16 @@
 // Immeddiately Invoked Function Expression
 
 (function chai(){
+    // named IIfE
     console.log("DB connected")
-})()
+})();
+
+(()=>{
+    console.log("Db connected 2")
+})();
+
+((name)=>{
+console.log(`MY name is ${name}`)
+})("Anish Sapkota")
 
 
