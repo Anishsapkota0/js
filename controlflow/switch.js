86634be2110b860a11acb9436e00512bbc939bfc
Anish sapkota <anishsapkota265@gmail.com>
2026-06-25 21:30:45 +0545

    thursday 3rd commit

---
+++ controlflow/switch.js
@@ -0,0 +1,13 @@
+let day = "sunda";
+switch(day){
+    case "sunday":
+        console.log("today is sunday")
+        break;
+    case "Monday":
+        console.log("today is monday")
+        break;
+    default:
+        console.log("other days")
+    
+
+}
