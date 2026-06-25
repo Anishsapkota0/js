6750a408ab9dd1955fd7f3052eb297de46d57d35
Anish sapkota <anishsapkota265@gmail.com>
2026-06-25 16:16:25 +0545

    Thursday 2nd commit

--- controlflow/one.js
+++ controlflow/one.js
@@ -1,5 +1,22 @@
-const isUserLoggedIn =true;
+// const temperature = 41
+// if (temperature<50){
+//     console.log("Temperature is less than 50")
+// }
 
-if(isUserLoggedIn){
-    console.log("my name is anish")
+
+// const score =200
+// if (score>100){
+//     const power="fly"
+//     console.log(`user power ${power}`)
+// }
+//   console.log(`user power ${power}`)
+
+const balance =1000
+if (balance>500) console.log("test")
+
+const userLoggedIn= true
+const debitCard = true
+
+if(userLoggedIn &&  debitCard){
+    console.log("allow to buy course")
 }
