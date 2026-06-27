b2fa3f0f0dc8b37cfb7ead669e4eaf86c10ec3ed
Anish sapkota <anishsapkota265@gmail.com>
2026-06-27 23:42:44 +0545

    saturday 3rd commit

--- iterations/one.js
+++ iterations/one.js
@@ -1,14 +1,32 @@
 //this is  iteration
 
-for (let i=0; i<=10; i++){
-    if(i==0){
+// for (let i=0; i<=10; i++){
+//     if(i==0){
 
+//     }
+//     else if(i==5){
+//         console.log(`${i} is the best number`)
+
+//     }else{
+//     console.log(i)
+//     }
+
+// }
+
+
+for(let i =0 ; i<=10;i++){
+    for (let j=0; j<=10;j++){
+        if (i==0){
+
+        }else if(i==1){
+        console.log(`${i}is ${i}st number`)
+        }else if(i==2){
+            console.log(`${i}is ${i}nd number`)
+        }else if(i==3){
+            console.log(`${i}is ${i}rd number`)
+        }else{
+            console.log(`${i}is ${i}th number`)
+
+        }
     }
-    else if(i==5){
-        console.log(`${i} is the best number`)
-
-    }else{
-    console.log(i)
-    }
-
 }
