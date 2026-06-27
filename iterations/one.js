e2dedfe8bc3bd2f093ec26c7990739b0da82ac56
Anish sapkota <anishsapkota265@gmail.com>
2026-06-28 00:02:23 +0545

    saturday 5th commit

--- iterations/one.js
+++ iterations/one.js
@@ -35,6 +35,6 @@ for (let i=1;i<=10;i++){
     console.log("----------------------------")
     console.log("Multiplication table of ",i)
     for (let j=1 ; j<=10 ; j++){
-        console.log(`${i} * ${j} = ${i*j}`)
+        console.log(`  ${i}   *   ${j}     =     ${i*j}   `)} 
+            
     }
-}
