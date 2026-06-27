22d8c4a084d8e479b1017d6bc269edb93e2ce561
Anish sapkota <anishsapkota265@gmail.com>
2026-06-27 23:55:58 +0545

    saturday 4th commit

--- iterations/one.js
+++ iterations/one.js
@@ -14,19 +14,27 @@
 // }
 
 
-for(let i =0 ; i<=10;i++){
-    for (let j=0; j<=10;j++){
-        if (i==0){
+// for(let i =0 ; i<=10;i++){
+//     for (let j=0; j<=10;j++){
+//         if (i==0){
 
-        }else if(i==1){
-        console.log(`${i}is ${i}st number`)
-        }else if(i==2){
-            console.log(`${i}is ${i}nd number`)
-        }else if(i==3){
-            console.log(`${i}is ${i}rd number`)
-        }else{
-            console.log(`${i}is ${i}th number`)
+//         }else if(i==1){
+//         console.log(`${i}is ${i}st number`)
+//         }else if(i==2){
+//             console.log(`${i}is ${i}nd number`)
+//         }else if(i==3){
+//             console.log(`${i}is ${i}rd number`)
+//         }else{
+//             console.log(`${i}is ${i}th number`)
 
-        }
+//         }
+//     }
+// }
+
+for (let i=1;i<=10;i++){
+    console.log("----------------------------")
+    console.log("Multiplication table of ",i)
+    for (let j=1 ; j<=10 ; j++){
+        console.log(`${i} * ${j} = ${i*j}`)
     }
 }
