6079c15029e7c8ff2c54095ec426684950b94c91
Anish sapkota <anishsapkota265@gmail.com>
2026-07-05 23:13:31 +0545

    sunday 2nd commit

--- function2.js
+++ function2.js
@@ -1,6 +1,17 @@
-function sum(a,b){
-    console.log(a+b)
+function sum(a,...b){
+    console.log(a,b)
 
 }
 
-sum(5,2)
+sum(5,2,3,4,5,6,7)
+
+
+const sumUpTo=(n)=>{
+    let sum =0
+    for(let i =0;i<=n;i++){
+        sum+=i
+    }
+    return sum
+}
+console.log(sumUpTo(30))
+
