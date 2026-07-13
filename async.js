ef842e28a94e4323080927a6bc9cabccc2268e7b
Anish sapkota <anishsapkota265@gmail.com>
2026-07-13 15:37:50 +0545

    monday 3rd commit

--- async.js
+++ async.js
@@ -4,3 +4,52 @@ for (let i in  nums){
     sum+=nums[i]
 }
 console.log(sum)
+nums.forEach((value,index)=>{
+    sum +=value
+
+})
+console.log(sum)
+
+const allProduct =[
+    {
+        name:"Product one",
+        price :1000,
+        discount:10
+    },
+    {
+        name:"Product Two",
+        price :1240,
+        discount: 7
+    },
+    {
+        name:"Product Three",
+        price :2350,
+        discount:7.5
+    }
+]
+
+const results = allProduct.map((value,index)=>{
+    return {
+        name : value.name,
+        discountPrice : (value.price)*(value.discount)/100,
+        priceAfterDiscount : value.price *(1-value.discount/100)
+    }
+
+})
+
+console.log(results)
+
+let higherDiscount = allProduct.filter((val)=>{
+    return val.discount>7
+
+})
+
+console.log(higherDiscount)
+
+let sum1= nums.reduce((prev,val,ind)=>{
+    
+    prev+=val
+    return prev
+
+},0)
+console.log(sum1)
