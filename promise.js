d591b3301edf6e9f7bfb845ee71d765eca6fbbc2
Anish sapkota <anishsapkota265@gmail.com>
2026-07-17 23:22:14 +0545

    friday 2nd commit

--- promise.js
+++ promise.js
@@ -1,6 +1,6 @@
-setTimeout(()=>{
-console.log("i am here")
-},3000)
+// setTimeout(()=>{
+// console.log("i am 8 years old")
+// },10000)
 
 
 // setInterval(()=>{
@@ -17,7 +17,124 @@ console.log("i am here")
  * settlement -----> Final 
  */
 
-let myvar =new Promise((resolve,reject)=>{
-    resolve()
+// let myvar =new Promise((resolve,reject)=>{
+//     resolve({name: "My name is sabal neupane"})
+
+// })
+// console.log(myvar.name)
+// myvar
+// .then((resolveData)=>{
+//     console.log(resolveData.name)
+// })
+// .catch((exception)=>{
+//     console.log(exception)
+
+// })
+
+const login=(username,password)=>{
+    return new Promise((res,rej)=>{
+        if(username=="admin" && password=="admin"){
+        res({
+            id : 1 ,
+            name : "Anish",
+            username: "admin",
+            address :" Hetauda"
+        })}
+        else{
+            rej({
+                error: "Invalid username or password"
+            })
+        }
+    })
+    }
+
+
+
+login("admin","admi")
+.then((resolveData)=>{
+    console.log(resolveData)
+})
+.catch((err)=>{
+    console.log(err)
 
 })
+
+login("admin","admin")
+.then((resolveData)=>{
+    console.log(resolveData)
+})
+.catch((err)=>{
+    console.log(err)
+
+})
+
+const functionONe=(data)=>{
+    return new Promise((res,rej)=>{
+        if(data){
+            res("Iam from function one")
+        }
+        else{
+            rej("I am reject from function one")
+        }
+
+    })
+}
+const functionTwo=(data)=>{
+    return new Promise((res,rej)=>{
+        if(data){
+            res("Iam from function Two")
+        }
+        else{
+            rej("I am reject from function Two")
+        }
+
+    })
+}
+
+const functionThree=(data)=>{
+    return new Promise((res,rej)=>{
+        if(data){
+            res("Iam from function Three")
+        }
+        else{
+            rej("I am reject from function Three")
+        }
+
+    })
+}
+
+
+
+functionONe(true)
+.then((resolveData)=>{
+    console.log(resolveData)
+
+})
+.catch((error)=>{
+    console.log(error)
+
+})
+
+
+functionTwo(false)
+.then((resolveData)=>{
+    console.log(resolveData)
+
+})
+.catch((error)=>{
+    console.log(error)
+
+})
+
+functionThree(true)
+.then((resolveData)=>{
+    console.log(resolveData)
+
+})
+.catch((error)=>{
+    console.log(error)
+
+})
+
+
+
