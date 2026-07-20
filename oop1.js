4e16695b5a65d2b1608497b79a33306487d8fe6f
Anish sapkota <anishsapkota265@gmail.com>
2026-07-21 00:01:22 +0545

    tuesday 1st commit

--- oop1.js
+++ oop1.js
@@ -5,6 +5,14 @@ class Person {
     email ='';
     phone = '';
 
+    constructor(_name,_address,_email,_phone){
+        this.name = _name
+        this.address = _address;
+        this.email =_email;
+        this.phone = _phone;
+
+    }
+
     setName(_name){
         this.name = _name
 
@@ -19,9 +27,21 @@ class Person {
 // Person.prototype.role = "user"
 
 
-const personObj = new Person()
-personObj.name = "Anish Sapkota"
-personObj.address = "Hetauda"
-personObj.email = "anishsapkota265@gmail.com"
-personObj.phone= "986828797"
-console.log(personObj)
+const personObj = new Person("anil Acharya","Dhading","anil@gmail.com","972763")
+// personObj.name = "Anish Sapkota"
+// personObj.address = "Hetauda"
+// personObj.email = "anishsapkota265@gmail.com"
+// personObj.phone= "986828797"
+console.log(personObj.setName("Gajendra basnet"))
+console.log(personObj.getName())
+
+
+
+
+// 4 Pillars of OOP
+/**
+ * Abstraction 
+ * Encapsulation
+ * Inheritance 
+ * polymorphism
+ */
